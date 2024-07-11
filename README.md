dcf230e20e6f1a6638c75532635bdf3a5f7cee54
top1c <afoninartofficial@gmail.com>
2024-07-11 14:18:07 +0300

    Update: Added babel + postcss

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # <a href="https://afoninis.github.io/viral-website/">Click to Watch</a><br />
+
 # <a href="https://viralisdignity.store/">Their website</a><br />
 
 Landing page for clothing brand "VIRAL"<br>
--- js/compiled-index.js
+++ js/compiled-index.js
@@ -1,54 +1,36 @@
 const AVAILABLE_SIZES = {
   M: 0,
   L: 5,
-  XL: 2,
+  XL: 2
 };
-
 const popup = document.querySelector("div.popup");
 const preloader = document.querySelector("div.preloader");
 const btnHeroBuy = document.querySelector("button.hero__button");
-
 const form = document.querySelector("form.form");
 const sizesBlock = document.querySelector("div.form__sizes");
 const soldoutLabel = document.querySelector("p.popup__sold-out");
-
 const itemsSizes = document.querySelectorAll("div.form__sizes-item");
 const inputsAmount = document.querySelectorAll("input.form__sizes-amount");
-
 let isShowPopup = false;
-
-if (
-  AVAILABLE_SIZES["M"] <= 0 &&
-  AVAILABLE_SIZES["L"] <= 0 &&
-  AVAILABLE_SIZES["XL"] <= 0
-) {
+if (AVAILABLE_SIZES["M"] <= 0 && AVAILABLE_SIZES["L"] <= 0 && AVAILABLE_SIZES["XL"] <= 0) {
   form.classList.add("hidden");
   soldoutLabel.classList.remove("hidden");
 } else {
   form.classList.remove("hidden");
   soldoutLabel.classList.add("hidden");
 }
-
-itemsSizes.forEach((el) =>
-  AVAILABLE_SIZES[el.dataset.size] <= 0
-    ? el.classList.add("hidden")
-    : el.classList.remove("hidden")
-);
-
-window.addEventListener("click", (e) => {
+itemsSizes.forEach(function (el) {
+  return AVAILABLE_SIZES[el.dataset.size] <= 0 ? el.classList.add("hidden") : el.classList.remove("hidden");
+});
+window.addEventListener("click", function (e) {
   if (isShowPopup) {
-    !e.target.closest("div.popup__wrapper") &&
-      !e.target.closest("div.buy__button") &&
-      !e.target.closest("button.hero__button") &&
-      togglePopup();
+    !e.target.closest("div.popup__wrapper") && !e.target.closest("div.buy__button") && !e.target.closest("button.hero__button") && togglePopup();
   }
 });
-
-window.addEventListener("keydown", (e) => {
+window.addEventListener("keydown", function (e) {
   if (e.keyCode == 27 && isShowPopup) togglePopup();
 });
-
-const togglePopup = () => {
+const togglePopup = function () {
   if (isShowPopup) {
     popup.style.display = "none";
   } else {
@@ -56,27 +38,15 @@ const togglePopup = () => {
   }
   isShowPopup = !isShowPopup;
 };
-
-document.querySelectorAll("p.form__sizes-option").forEach((el) => {
-  el.addEventListener("click", (e) => {
+document.querySelectorAll("p.form__sizes-option").forEach(function (el) {
+  el.addEventListener("click", function (e) {
     const clickedOption = e.target;
-    if (
-      clickedOption
-        .closest("div.form__sizes-item")
-        .querySelector("input.form__sizes-amount")
-        .classList.contains("hidden")
-    ) {
+    if (clickedOption.closest("div.form__sizes-item").querySelector("input.form__sizes-amount").classList.contains("hidden")) {
       el.classList.add("form__option-toggled");
-      clickedOption
-        .closest("div.form__sizes-item")
-        .querySelector("input.form__sizes-amount")
-        .classList.remove("hidden");
+      clickedOption.closest("div.form__sizes-item").querySelector("input.form__sizes-amount").classList.remove("hidden");
     } else {
       el.classList.remove("form__option-toggled");
-      clickedOption
-        .closest("div.form__sizes-item")
-        .querySelector("input.form__sizes-amount")
-        .classList.add("hidden");
+      clickedOption.closest("div.form__sizes-item").querySelector("input.form__sizes-amount").classList.add("hidden");
     }
   });
 });
